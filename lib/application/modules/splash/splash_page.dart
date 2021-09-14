a667d6c56b8b0f9c49f996d234a3fe66cb4a6303
bruno <bruno@colaco.com.br>
2021-09-14 00:02:52 -0300

    Primeira aula

---
+++ lib/application/modules/splash/splash_page.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Center(
+          child: Container(
+              child: Text(RemoteConfig.instance.getString('api_token')))),
+    );
+  }
+}
